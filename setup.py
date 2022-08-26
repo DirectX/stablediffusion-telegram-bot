0a8d5fd3a5024ceb364f0166ebf8fe7161f71082
DirectX <DirectX@users.noreply.github.com>
2022-08-26 18:02:10 +0300

    Fixed setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='stablediffusion-telegram-bot',
     version='1.0',
     description='Telegram bot for Stable Diffusion neural network',
     author='DirectX',
     packages=['app'],
-    install_requires=['argostranslate', 'python-dotenv', 'python-telegram-bot'],
+    install_requires=required,
     project_urls={
         "Source Code": "https://github.com/DirectX/stablediffusion-telegram-bot"
     },
